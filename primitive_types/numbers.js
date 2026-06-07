f05ab6684d921d3aea370d228b907015fb36d7ae
Clayton Feathers <claytonfeathers@live.com>
2026-06-06 23:23:16 -0400

    update numbers tutorial

--- primitive_types/numbers.js
+++ primitive_types/numbers.js
@@ -1,5 +1,3 @@
-logger.debug("------------")
-
 // numbers lesson
 var _int = 1234		// floating point float
 var _float = 1234.54	// float
@@ -23,27 +21,6 @@ logger.debug(Number.isInteger(9007199254740991)) // true
 logger.debug(Number.isInteger(9007199254740992)) // true, it looks at the
 logger.debug(Number.isInteger(-9007199254740991)) // true
 logger.debug(Number.isInteger(-9007199254740992)) // false, because isSafeInteger looks at the actual value
-logger.debug(Number.isSafeInteger(1.00000000000000009))	// true because 1e-16 (16 decimal places) is too small to be represented
-logger.debug(Number.isSafeInteger(1.0000000000000009))		// false because it's more than 1e-16 (its 1e-15)
-
-/*
-isNaN()
-Checks if a value is NaN, doesn't convert strings to numbers so it's always false
-*/
-logger.debug("abc: ".concat(Number.isNaN("abc"))) // false
-logger.debug("abc as number: ".concat(Number.isNaN(Number("abc")))) // false
-logger.debug("1234 string: ".concat(Number.isNaN("1234")))
-logger.debug("1234 number: ".concat(Number.isNaN(1234)))
-
-/*
-isSafeInteger()
-Integer is any non-decimal value (kinda) number between -9007199254740991 and +9007199254740991
-Returns true or false
-*/
-logger.debug(Number.isSafeInteger(9007199254740991)) // true
-logger.debug(Number.isSafeInteger(9007199254740992)) // false, because isSafeInteger looks at the actual value
-logger.debug(Number.isSafeInteger(-9007199254740991)) // true
-logger.debug(Number.isSafeInteger(-9007199254740992)) // false, because isSafeInteger looks at the actual value
 
 /*
 parseFloat()
@@ -68,14 +45,6 @@ Instance Methods for Number
 --------------------------------- */
 var _int = 12345678
 var _float = 1234.5678
-
-/*
-toExponential()
-Returns the exponetial version of the number (123456 with 2 -> 1.23e+5)
-*/
-logger.debug(_int.toExponential(2)) 	// 1.23e+7
-logger.debug(_int.toExponential(4)) 	// 1.2345e+7
-logger.debug(_float.toExponential(2))	// 1.23e+3
 
 /*
 toFixed()
@@ -109,3 +78,36 @@ gets the string version of the number, can be used to specify a base
 logger.debug(_int.toString()) 	// string version so "12345678"
 logger.debug(_int.toString(2))		// binary version (101111000110000101001110)
 logger.debug(Number(2).toString(2))	// easier binary version (10)
+
+
+/*
+    Number Calculations
+*/
+
+/*
+adding
+*/
+var n = 2
+
+logger.debug(n + 2)     // 4.0
+
+// ++
+n++
+logger.debug(n)          // 3.0
+
+n = 2 // reset
+logger.debug(n++)       // 2.0 - because ++ runs after the logging statement
+logger.debug(n++)       // 3.0 - because ++ runs after the logging statement
+
+n = 2 // reset
+logger.debug(++n)       // 3.0
+logger.debug(++n)       // 4.0
+
+// +=
+n = 2 // reset
+n += 3 // n = n + 3
+logger.debug(n)         // 5.0
+
+// -
+n = 2 // reset
+logger.debug(n-1)       // 1.0
